e0ae03317b7bb973007f323b583c950ef4f64ec5
Carvax2 <62867091+Carvax2@users.noreply.github.com>
2023-11-16 20:07:54 -0300

    Agregado footer

--- FINALREACT-SEGUNDAENTREGA/src/App.js
+++ FINALREACT-SEGUNDAENTREGA/src/App.js
@@ -5,21 +5,26 @@ import ItemListContainer from './components/ItemlListContainer/ItemListContainer
 import ItemCount from './components/ItemCount/ItemCount';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import { CartProvider } from './CartContext/CartContext';
+import { Footer } from './components/Footer/Footer';
 
 
 function App() {
   return (
 
     <div className="App">
-      <BrowserRouter>
+      <CartProvider>
+      <BrowserRouter className="App">
       <Navbar/>
       <Routes>
-        <Route path="/" element={<ItemListContainer />} />
+        <Route path="/" exact element={<ItemListContainer />} />
         <Route path="/category/:categoryId" element={<ItemListContainer />} />
         <Route path="/item/:itemid" element={<ItemDetailContainer />} />
         <Route path="*" element={<h1>404 NOT FOUND</h1>} />
       </Routes>
       </BrowserRouter>
+      </CartProvider>
+      <Footer />
     </div>
   );
 }
