72b7f47140b264408ca750a78becf2b708998536
Carvax2 <62867091+Carvax2@users.noreply.github.com>
2023-11-17 16:31:57 -0300

    solucionado item undefined

--- FINALREACT-SEGUNDAENTREGA/src/components/Cart/Cart.js
+++ FINALREACT-SEGUNDAENTREGA/src/components/Cart/Cart.js
@@ -3,12 +3,14 @@ import { CartContext } from "../../context/CartContext";
 import { Link } from "react-router-dom";
 import CartItem from "../CartItem/CartItem";
 
+
+
 const Cart = ()=> {
     const {cart,  clearCart, cartTotal, totalQuantity} = useContext(CartContext);
 
     if (totalQuantity ===0){
         return (
-            <div className="cart">
+            <div>
                 <h2>El carrito está vacio</h2>
                 <Link to="/">
                     <button>Continuar comprando</button>
@@ -16,20 +18,23 @@ const Cart = ()=> {
             </div>
         );
     }
+else {
+
 
     return (
-        <div className="cart">
+        <div>
             <h2>TU CODER CARRITO</h2>
             <div>
-            {cart.map (p => <CartItem key={p.id}{...p}/>)}
+            {cart.map (p => <CartItem key={p.id} item={p} cartTotal={cartTotal} {...p}/>)}
             <h3>Total: ${cartTotal}</h3>
             <button onClick={()=>(clearCart())}>Vaciar carrito</button>
             <Link to={'/checkout'}>Checkout</Link>
             </div>
         </div>
 
-        
+    
     );
+}
 }
 
 export default Cart;
