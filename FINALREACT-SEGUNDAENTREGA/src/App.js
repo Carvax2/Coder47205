88d0565d41caaf4d15c5f45ec9629ca6e95d91f4
Carvax2 <62867091+Carvax2@users.noreply.github.com>
2023-11-16 22:06:55 -0300

    Add files via upload

--- FINALREACT-SEGUNDAENTREGA/src/App.js
+++ FINALREACT-SEGUNDAENTREGA/src/App.js
@@ -5,10 +5,11 @@ import ItemListContainer from './components/ItemlListContainer/ItemListContainer
 import ItemCount from './components/ItemCount/ItemCount';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
-import { CartProvider } from './context/CartContext';
+import { CartProvider, CartContext } from './context/CartContext';
 import { Footer } from './components/Footer/Footer';
 import { useHistory } from "react-router-dom";
 import CheckOut from './components/CheckOut/CheckOut';
+import Cart from './/components/Cart/Cart';
 
 
 function App() {
@@ -22,7 +23,7 @@ function App() {
         <Route path="/" exact element={<ItemListContainer />} />
         <Route path="/category/:categoryId" element={<ItemListContainer />} />
         <Route path="/item/:itemId" element={<ItemDetailContainer />} />
-        <Route path="/cart" element={<cart />} />
+        <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={ <CheckOut />} />
         <Route path="*" element={<h1>404 NOT FOUND</h1>} />
       </Routes>
